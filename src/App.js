f328b381ff3a18af88559ebce730faef21279513
Gokul Gopi <68158600+Gokul-Gopi@users.noreply.github.com>
2020-12-17 12:26:21 +0530

    refactor: added more food items

--- src/App.js
+++ src/App.js
@@ -17,7 +17,24 @@ let dataBase = {
   "🌭": "Hot Dog",
   "🌯": "Burito",
   "🥜": "Peanuts",
-  "🥥": "Coconut"
+  "🥥": "Coconut",
+  "🍇": "Grapes",
+  "🍞": "Bread",
+  "🌶️": "Hot Pepper",
+  "🍗": "Poultry Leg",
+  "🍈": "Melon",
+  "🍙": "Rice Ball",
+  "🍊": "Tangerine",
+  "🥝": "Kiwi Frurit",
+  "🌽": "Ear of Corn",
+  "🌰": "Chesnut",
+  "🥖": "Baguette Bread",
+  "🍟": "French Fries",
+  "🥞": "Pancakaes",
+  "🧇": "Wasffle",
+  "🧆": "Falafel",
+  "🍝": "Spaghetti",
+  "🦪": "Oyster"
 };
 
 let foodItems = Object.keys(dataBase);
@@ -40,10 +57,21 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1>Know IT before you Eat IT!</h1>
-      <input onChange={changeEventHandler} />
+      <h1 style={{ margin: "40px", color: "white" }}>
+        Know IT before you Eat IT!
+      </h1>
+      <hr />
+      <input
+        style={{ borderRadius: "5px", marginTop: "20px" }}
+        placeholder="put your input here or click on any of the item below"
+        onChange={changeEventHandler}
+      />
 
-      <div style={{ margin: "10px", fontSize: "20px" }}>{items}</div>
+      <div
+        style={{ marginBottom: "40px", marginTop: "30px", fontSize: "20px" }}
+      >
+        <strong>{items}</strong>
+      </div>
       <span style={{ margin: "20px", textDecoration: "underline" }}>
         Food items
       </span>
@@ -59,7 +87,7 @@ export default function App() {
               cursor: "pointer",
               fontSize: "25px",
               marginTop: "5px",
-              width: "120px"
+              width: "110px"
             }}
           >
             {eachItem}
